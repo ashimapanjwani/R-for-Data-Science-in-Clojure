d18ae58b47af0f13f922c4836359d0d30c1cb848
33724707 <ashimapanjwani@gmail.com>
2021-01-18 02:25:15 +0530

    deleted sample code

--- src/r4ds_in_clj/core.clj
+++ src/r4ds_in_clj/core.clj
@@ -1,16 +1,1 @@
-(ns r4ds-in-clj.core
-  (:require [tablecloth.api :as api]
-            [tech.v3.datatype.datetime]
-            [tech.v3.datatype.functional]
-            [notespace.api :as notespace]
-            [notespace.kinds :as kind]))
-
-^kind/dataset
-(-> "https://raw.githubusercontent.com/techascent/tech.ml.dataset/master/test/data/stocks.csv"
-    (api/dataset {:key-fn keyword})
-    (api/group-by (fn [row]
-                    {:symbol (:symbol row)
-                     :year (tech.v3.datatype.datetime/long-temporal-field :years (:date row))}))
-    (api/aggregate #(tech.v3.datatype.functional/mean (% :price)))
-    (api/order-by [:symbol :year])
-    (api/head 10))
+(ns r4ds-in-clj.core)
