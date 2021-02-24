5b777c5d65349bcf034d529c4aba6671d9cba1d4
ashimapanjwani <ashimapanjwani@gmail.com>
2021-02-25 01:02:09 +0530

    updated dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [scicloj/notespace "3-alpha3-SNAPSHOT"]
-                 [scicloj/tablecloth "5.00-beta-21a"]
-                 [scicloj/clojisr "1.0.0-BETA16"]
+                 [scicloj/notespace "3-beta3"]
+                 [scicloj/tablecloth "5.00-beta-29.1"]
+                 [scicloj/clojisr "1.0.0-BETA18"]
                  [aerial.hanami "0.12.4"]
                  [techascent/tech.viz "0.4.3"]
-                 [generateme/fastmath "2.0.5"]]
+                 [generateme/fastmath "2.1.1"]]
   :repl-options {:init-ns r4ds-in-clj.core})
