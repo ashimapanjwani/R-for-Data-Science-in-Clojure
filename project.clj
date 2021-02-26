ad32fc09f7e8da4851567ed8d7b12ff30634373b
ashimapanjwani <ashimapanjwani@gmail.com>
2021-02-27 02:49:26 +0530

    updated description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject r4ds-in-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A beginners guide to data science in Clojure."
+  :url "https://github.com/ashimapanjwani/r-for-data-science-in-clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins [[lein-tools-deps "0.4.5"]]
