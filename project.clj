36ca55fbc9c2022d639c236deb1348f9efbbbf72
ashimapanjwani <ashimapanjwani@gmail.com>
2021-02-27 02:45:52 +0530

    added dependencies to a deps.edn file

--- project.clj
+++ project.clj
@@ -3,11 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [scicloj/notespace "3-beta3"]
-                 [scicloj/tablecloth "5.00-beta-29.1"]
-                 [scicloj/clojisr "1.0.0-BETA18"]
-                 [aerial.hanami "0.12.4"]
-                 [techascent/tech.viz "0.4.3"]
-                 [generateme/fastmath "2.1.1"]]
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :repl-options {:init-ns r4ds-in-clj.core})
